83efd7ccdc7154f1b75ef6d46a848b3c40988c01
Efrain A. Davila <105945226+EfrainAD@users.noreply.github.com>
2023-07-12 23:44:10 -0400

    Code Wars - Next bigger number with the same digits V2 no brute force

--- Code_Wars/Next-bigger-number-with-the-same-digits.js
+++ Code_Wars/Next-bigger-number-with-the-same-digits.js
@@ -1,32 +1,48 @@
+const flip = (arr, a, b) => ([arr[a], arr[b]] = [arr[b], arr[a]])
+
 const nextBigger = (n) => {
-   const numInArr = String(n).split('')
-   const largest = Number(numInArr.sort((a, b) => b - a).join(''))
+   const numInArr = [...String(n)]
+   let answer = -1
 
-   for (let i = n + 1; i <= largest; i++) {
-      const digitBank = [...numInArr]
-      const number = String(i).split('')
-      const numOfRounds = number.length
+   const endNums = []
+   for (
+      let currentIndex = numInArr.length - 1;
+      currentIndex >= 0;
+      currentIndex--
+   ) {
+      endNums.push({ value: numInArr[currentIndex], index: currentIndex })
 
-      for (let j = 0; j < numOfRounds; j++) {
-         if (number.includes(digitBank[digitBank.length - 1])) {
-            const search = digitBank.pop()
-            number.splice(number.indexOf(search), 1)
-         } else {
-            break
-         }
+      const nextNumber = {
+         value: numInArr[currentIndex - 1],
+         index: currentIndex - 1,
       }
-      if (digitBank.length === 0) {
-         return i
+
+      const find = endNums.find((number) => nextNumber.value < number.value)
+
+      if (find) {
+         flip(numInArr, nextNumber.index, find.index)
+
+         const front = numInArr.slice(0, nextNumber.index + 1)
+         const last = numInArr.slice(nextNumber.index + 1).sort()
+
+         answer = Number([...front, ...last].join(''))
+
+         break
       }
    }
-   return -1
+   return answer
 }
 
 const tests = [
    [12, 21],
    [513, 531],
    [2017, 2071],
+   [23864, 24368],
    [414, 441],
+   [1234567899, 1234567989],
+   [1234567890, 1234567908],
+   [9876543210, -1],
+   [59884848459853, 59884848483559],
 ]
 
 tests.forEach((test) => console.log(nextBigger(test[0]) === test[1]))
