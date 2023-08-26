59a76b831b73391bfa717fa228276e2e6ee8ec50
Efrain A. Davila <105945226+EfrainAD@users.noreply.github.com>
2023-08-26 13:13:40 -0400

    took out the code . part. better that I do that then run the code to open the file

--- createcodechallenge.sh
+++ createcodechallenge.sh
@@ -4,7 +4,7 @@
 input="$(echo "$1" | xargs)"
 
 # Convert input string to filename format
-filename=$(echo "$input" | tr '[:upper:]' '[:lower:]' | sed -E 's/[^a-z0-9]+/-/g' | sed 's/-$//')
+filename=$(echo "$input" | sed -E 's/[^a-zA-Z0-9]+/-/g' | sed 's/-$//')
 
 # Create JavaScript file
 echo -e "\n\n// Test\nconst testCases = [\n\t\n]\n\ntestCases.forEach(test => console.log( (test.input) === test.output) )" > "$filename.js"
