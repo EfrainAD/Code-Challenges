43599840bb0b231bf20ba673acebec45c4b61c90
Efrain A. Davila <105945226+EfrainAD@users.noreply.github.com>
2023-10-13 13:11:26 -0400

    HackerRank - Day 2 of 30 Day Challenge
    Take away:
       1. in Hacke Rank I can use readLine() though it's no good in node.js strange, it really should tell me.
       2. Was a reminder that strin come in as str even if it is a number.

---
+++ Hacker_Rank/30-data-types.js
@@ -0,0 +1,20 @@
+/* 
+Notes:
+1. in Hacke Rank I can use readLine() though it's no good in node.js strange, it really should tell me.
+2. Was a reminder that strin come in as str even if it is a number.
+*/
+// Declare second integer, double, and String variables.
+let int = null
+let double = null
+let str = null
+// Read and save an integer, double, and String to your variables.
+int = parseInt(readLine())
+double = parseFloat(readLine())
+str = readLine()
+// Print the sum of both integer variables on a new line.
+console.log(parseInt(int + i))
+// Print the sum of the double variables on a new line.
+console.log((double + d).toFixed(1))
+// Concatenate and print the String variables on a new line
+// The 's' variable above should be printed first.
+console.log(s + str)
