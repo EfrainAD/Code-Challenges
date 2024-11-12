6df9fae8b7e3a5f38c9f3442d91dedcfac392653
Efrain A. Davila <105945226+EfrainAD@users.noreply.github.com>
2024-11-12 01:05:52 -0500

    HackerRank - 6 problems from 10 Days of Javascript

---
+++ Hacker_Rank/10_Days_of_Javascript/Day-03-Arrays.js
@@ -0,0 +1,57 @@
+'use strict'
+
+process.stdin.resume()
+process.stdin.setEncoding('utf-8')
+
+let inputString = ''
+let currentLine = 0
+
+process.stdin.on('data', (inputStdin) => {
+   inputString += inputStdin
+})
+
+process.stdin.on('end', (_) => {
+   inputString = inputString
+      .trim()
+      .split('\n')
+      .map((string) => {
+         return string.trim()
+      })
+
+   main()
+})
+
+function readLine() {
+   return inputString[currentLine++]
+}
+
+/**
+ *   Return the second largest number in the array.
+ *   @param {Number[]} nums - An array of numbers.
+ *   @return {Number} The second largest number in the array.
+ **/
+function getSecondLargest(nums) {
+   // Complete the function
+   return nums.reduce(
+      (obs, number) => {
+         if (number !== obs.largest) {
+            if (number > obs.largest) {
+               obs.secondLargest = obs.largest
+               obs.largest = number
+            } else if (number > obs.secondLargest) {
+               obs.secondLargest = number
+            }
+         }
+         return obs
+      },
+      { largest: -Infinity, secondLargest: -Infinity }
+   ).secondLargest
+   //return res.secondLargest
+}
+
+function main() {
+   const n = +readLine()
+   const nums = readLine().split(' ').map(Number)
+
+   console.log(getSecondLargest(nums))
+}
